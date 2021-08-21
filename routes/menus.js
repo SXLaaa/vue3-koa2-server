227f41994dc52acfd53689e2abc1e616e3a8a56c
shixl <shixl@dist.com.cn>
2021-08-22 01:12:53 +0800

    8-19菜单列表递归实现

--- routes/menus.js
+++ routes/menus.js
@@ -3,6 +3,41 @@ const util = require("../utils/utils");
 const Menu = require("../models/menuSchema");
 
 router.prefix("/menu");
+// 菜单列表查询
+router.get("/list", async (ctx) => {
+  const { menuName, menuState } = ctx.request.query;
+  const params = {};
+  if (menuName) params.menuName = menuName;
+  if (menuState) params.menuState = menuState;
+  let rootList = (await Menu.find(params)) || [];
+  // rootList就是数据库中的menus表
+  const permissionList = getTreeMenu(rootList, null, []);
+  ctx.body = util.success(permissionList);
+});
+// 递归拼接树形列表
+function getTreeMenu(rootList, id, list) {
+  for (let i = 0; i < rootList.length; i++) {
+    let item = rootList[i];
+    if (String(item.parentId.slice().pop()) == String(id)) {
+      // slice 不会改变原数组； pop尾部删除，会改变原数组
+      // 判断数组第一个值是null，就说明是父级菜单
+      console.log(item._doc);
+      list.push(item._doc); // _doc 指的是文档，当前这条数据; moongos语法，取子文档
+    }
+  }
+  list.map((item) => {
+    item.children = [];
+    getTreeMenu(rootList, item._id, item.children);
+    if (item.children.length == 0) {
+      delete item.children;
+    } else if (item.children.length > 0 && item.children[0].menuType == 2) {
+      //menuType == 2 按钮类型
+      item.action = item.children;
+    }
+  });
+  return list;
+}
+//菜单编辑、新增、删除
 router.post("/operate", async (ctx) => {
   const { _id, action, ...params } = ctx.request.body;
   let res, info;
